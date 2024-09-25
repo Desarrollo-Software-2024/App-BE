56cc69baa81629f86236d4f6fb13f6237b860f37
franciscobournissen <franciscobournissen@gmail.com>
2024-09-24 21:34:50 -0300

    Dependencias

--- src/Netflis.Domain.Shared/obj/Debug/net8.0/Netflis.Domain.Shared.AssemblyInfo.cs
+++ src/Netflis.Domain.Shared/obj/Debug/net8.0/Netflis.Domain.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Netflis.Domain.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2996b8b56c2bdf3f280eb0abef200db31f1966d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b96d8587a1f1c7d1b6525d4331b340367b9508d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Netflis.Domain.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Netflis.Domain.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
